e03aac1efd7cb299a66bd485afc7e394513c47bc
samuelsrmv <samuel.srmv@gmail.com>
2021-02-20 12:13:57 -0500

    Arbnb

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,30 @@
 #!/usr/bin/python3
-"""class Base Model"""
-from datetime import datetime
+"""The class"""
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """Clase Base
-    """
-    
+    """Class BaseModel"""
     format_date = '%Y-%m-%dT%H:%M:%S.%f'
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     
-    def __init__(self, *args, **kwargs):
-        """Inicializando atributos públicos en la clase
-        id = contiene un identificador único
-        created_at = fecha y hora del objeto
-        updated_at = fecha de actualización y hora del objeto
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value, self.format_date)
-                elif key == '__class__':
-                    pass
-                else:
-                    self.__dict__[key] = value
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-
     def __str__(self):
-        """
-        Returns
-            [str]: [information of the class]
-        """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update
-        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Returns:
-            a_dict: containg all information of the class
-        """
-        cpy_dict = dict(self.__dict__)
-        cpy_dict['__class__'] = type(self).__name__
-
-        for key, value in cpy_dict.items():
-            if isinstance(value, datetime):
-                cpy_dict[key] = value.strftime(self.format_date)
-
-        return cpy_dict
-    
+        dict_ = {}
+        dict_.update({'__class__': type(self).__name__})
+        for key in self.__dict__:
+            if key == ['updated_at', 'created_at']:
+                dict_.update({key: datetime.isoformat(getattr(self, key))})
+            else:
+                dict_.update({key: getattr(self, key)})
+        return dict_
