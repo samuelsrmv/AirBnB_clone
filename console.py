780312595e8dcaf68f6d54bec67e52cc63985268
samuelsrmv <samuel.srmv@gmail.com>
2021-02-21 16:28:11 -0500

    console.py

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""The console"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """[This is the htbn cls]
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """quit: quit
+    Quit the console."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF: ^C
+    Exit the console."""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
